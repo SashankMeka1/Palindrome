3987c257aa8e90f6331ecd7e24acca0792ba28f3
Sashank <sashankmeka7@gmail.com>
2021-02-21 23:24:35 -0800

    fixes

--- palindrome.cpp
+++ palindrome.cpp
@@ -3,26 +3,20 @@
 using namespace std;
 bool issame(char input[], int length, int left_index, int right_index){
     if (left_index < length){
-        int front = isalnum(input[left_index])/8;
+        int front = isalnum(input[left_index])/8;//we dont actually remove the punctuation, we just skip the punctuation because it is not alphanumeric and check the next item in the cstring
         int back = isalnum(input[right_index])/8;//check if alphanumeric and divide by eight to normalize
-	if (front + back == 2 and input[left_index] == input[right_index]){//if alpha numeric and equal go to next
-            return issame(input, length, left_index + 1, right_index - 1);
+        if(front + back == 2 and input[left_index] != input[right_index]){
+            	return false;//if unequal return false
         }
-        else if (front + back == 0){//if not alpha numeric skip
-            return issame(input, length, left_index + 1, right_index - 1);
-        }
-        else if (front + back == 1){//if one not alphanumeric skip that one only
-            return issame(input, length, left_index + back, right_index - front);
-        }
-        else if(front + back == 2 and input[left_index] != input[right_index]){
-            return false;//if unequal return false
-        }
+	else{//close in on both sides checking if equal
+		return issame(input, length, left_index + back, right_index - front);
+	}
     }
     return true; 
 }
 int main(){
     char input[80];//grab input
-    cin>>input;
+    cin.get(input,80);
     bool palindrome = issame(input, strlen(input), 0, strlen(input)-1);
     if (palindrome){//use palindrome function to check
 	cout << "It is a palindrome"<<endl;
